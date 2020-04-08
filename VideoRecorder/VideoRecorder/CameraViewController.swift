abb7d76f4c3bcab6ed0800ae037ecab0a36f5b89
Paul Solt <PaulSolt@gmail.com>
2020-04-08 13:02:14 -0400

    Added Video Preview

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -7,20 +7,82 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+    lazy private var captureSession = AVCaptureSession()
+    
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
+        // Resize camera preview to fill the entire screen
+        cameraView.videoPlayerView.videoGravity = .resizeAspectFill
+        
+        setUpCaptureSession()
+    }
 
-		// Resize camera preview to fill the entire screen
-		cameraView.videoPlayerView.videoGravity = .resizeAspectFill
-	}
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        captureSession.startRunning()
+    }
 
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        captureSession.stopRunning()
+    }
+    
+    
+    private func setUpCaptureSession() {
+        
+        captureSession.beginConfiguration()
+        
+        // Add inputs
+        let camera = bestCamera()
+        
+        // Video
+        guard let captureInput = try? AVCaptureDeviceInput(device: camera),
+            captureSession.canAddInput(captureInput) else {
+                fatalError("Can't create the input form the camera")
+        }
+        captureSession.addInput(captureInput)
+        
+        if captureSession.canSetSessionPreset(.hd1920x1080) { // FUTURE: Play with 4k
+            captureSession.sessionPreset = .hd1920x1080
+        }
+        
+        // Audio
+        
+        
+        // Add outputs
+        
+        // Recording to disk
+        
+        captureSession.commitConfiguration()
+        
+        // Live preview
+        cameraView.session = captureSession
+    }
+
+    private func bestCamera() -> AVCaptureDevice {
+        // All iPhones have a wide angle camera (front + back)
+        if let ultraWideCamera = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return ultraWideCamera
+        }
+        
+        if let wideCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return wideCamera
+        }
+        
+        // Future: Add a button to toggle front/back camera
+        
+        fatalError("No cameras on the device (or you're running this on a Simulator which isn't supported)")
+    }
 
     @IBAction func recordButtonPressed(_ sender: Any) {
 
